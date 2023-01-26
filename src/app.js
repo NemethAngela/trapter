ddf558fc4cbf60f04d4c5932300e4f57c537a72a
NemethAngela <a.nemeth75@yahoo.com>
2023-01-26 19:32:37 +0100

    Webes UI kész

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+* File: app.js
+* Author: Németh Angéla
+* Copyright: 2023, Németh Angéla
+* Group: Szoft 1-2 E
+* Date: 2023-01-26
+* Github: https://github.com/NemethAngela/
+* Licenc: GNU GPL
+*/
