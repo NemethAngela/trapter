abf70dfa01a0e7fccbcd1043e141f2d4d0a901b5
NemethAngela <a.nemeth75@yahoo.com>
2023-01-26 20:13:08 +0100

    JavasSricpt kész

--- src/app.js
+++ src/app.js
@@ -7,3 +7,35 @@
 * Github: https://github.com/NemethAngela/
 * Licenc: GNU GPL
 */
+//kötések a html-hez:
+const asideElem = document.querySelector("#aside");
+const bsideElem = document.querySelector("#bside");
+const csideElem = document.querySelector("#cside");
+const dsideElem = document.querySelector("#dside");
+const calcButton = document.querySelector("#calcButton");
+const areaElem = document.querySelector("#area"); //nyomógomb
+
+//eseménykezelő, hgy mire reagálunk: klikk
+calcButton.addEventListener('click', () => {
+    console.log('működik');
+    let aside = Number(asideElem.value);
+    let bside = Number(bsideElem.value);
+    let cside = Number(csideElem.value);
+    let dside = Number(dsideElem.value);
+    let area = calcArea(aside, bside, cside, dside);
+    areaElem.value = area;
+    
+});
+
+function calcArea(a, b, c, d) {
+    let terulet = ( (a + c)/(4*(a - c))) 
+    * Math.sqrt(
+        (a+b-c+d)*
+        (a-b-c+d)*
+        (a+b-c-d)*
+        (-a+b+c+d)
+    )
+    return terulet;
+
+}
+//+ függvény a számításhoz:
